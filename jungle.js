4ae629f41b0fe9a0fa01dc42b4b6de5872f89046
Julia <z3211232001@gmail.com>
2025-01-17 11:21:33 -0800

    finished

--- jungle.js
+++ jungle.js
@@ -1,17 +1,154 @@
 class Animal {
+  #name
+  #species
+  #energy
+  #decreaseAmount = 10
+  #increaseAmount = 10 // 先預設增加減少的能量
+  static remainingAnimals = 0
 
+  constructor(name, species, energy) {
+    this.#name = name
+    this.#species = species
+    this.#energy = energy
+    Animal.remainingAnimals++
+  } 
+
+  get name() {
+    return this.#name
+  }
+
+  set name(value) {
+    this.#name = value
+  }
+
+  get species() {
+    return this.#species
+  }
+
+  set species(value) {
+    this.#species = value
+  }
+
+  get energy() {
+    return this.#energy
+  }
+
+  set energy(value) {
+    this.#energy = value
+  }
+
+  get decreaseAmount() {
+    return this.#decreaseAmount
+  }
+
+  set decreaseAmount(decreaseAmount) {
+    this.#decreaseAmount = decreaseAmount
+  }
+
+  get increaseAmount() {
+    return this.#increaseAmount
+  }
+
+  set increaseAmount(increaseAmount) {
+    this.#increaseAmount = increaseAmount
+  }
+
+  attack(target) {
+    this.#energy -= this.#decreaseAmount
+    target.energy -= this.#decreaseAmount
+    
+    if(this.#energy <= 0) {
+      console.log(`${target.name} won`)
+      Animal.remainingAnimals --
+    }
+
+    if(target.energy <= 0) {
+      console.log(`${this.#name} won`)
+      Animal.remainingAnimals --
+    }
+
+    console.log(`${this.#name}'s energy: ${this.#energy}`)
+    console.log(`${target.name}'s energy: ${target.energy}`)
+
+  }
+
+  eat() {
+    this.#energy += this.#increaseAmount
+
+    console.log(`${this.#name} eats and gains energy! `)
+    console.log(`${this.#name}'s energy: ${this.#energy}`)
+  }
 }
 
 class Bird extends Animal {
-  
+  #canFly
+
+  constructor(name, species, canFly) {
+    super(name, species, 100)
+    this.#canFly = canFly
+    this.decreaseAmount = 20;
+    this.increaseAmount = 10;
+  }
+
+  get canFly() {
+    return this.#canFly
+  }
+
+  set canFly(value) {
+    this.#canFly = value
+  }
+
+  attack(target) {
+    console.log(`${this.name} swoops in to attack ${target.name}!`)
+    super.attack(target)
+  }
 }
 
 class Mammal extends Animal {
+  #furColor
 
+  constructor(name, species, furColor) {
+    super(name, species, 200)
+    this.#furColor = furColor
+    this.decreaseAmount = 50
+    this.increaseAmount = 20
+  }
+
+  get furColor() {
+    return this.#furColor
+  }
+
+  set furColor(value) {
+    this.#furColor = value
+  }
+
+  attack(target) {
+    console.log(`${this.name} lunges to attack ${target.name}!`)
+    super.attack(target)
+  }
 }
 
 class Reptile extends Animal {
+  #coldBlooded
 
+  constructor(name, species, coldBlooded) {
+    super(name, species, 100)
+    this.#coldBlooded = coldBlooded
+    this.decreaseAmount = 30
+    this.increaseAmount = 15
+  }
+  get coldBlooded() {
+    return this.#coldBlooded
+  }
+
+  set coldBlooded(value) {
+    this.#coldBlooded = value
+  }
+
+  attack(target) {
+    console.log(`${this.name} bites to attack ${target.name}!`)
+    super.attack(target)
+  }
 }
 
 
